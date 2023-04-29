65ba3fa6889c0f0e29335017a04a8ffb7ba6ffe2
SiMoZ9 <simonejonas08@gmail.com>
2023-04-29 18:26:42 +0200

    automa2_nfa

--- automa2_nfa.py
+++ automa2_nfa.py
@@ -1,2 +1,13 @@
 class NFA:
-    pass
+    def __init__(self):
+        self.states = {'q0', 'q1', 'q2', 'q3'}
+        self.alfa = {'0', '1'}
+        self.initial_state = self.states[0]
+        self.current_state = self.initial_state
+    
+    def transition(self, chr):
+        if chr == '0' and self.current_state == 'q0':
+            self.current_state = 'q0'
+        
+        if chr == '1' and self.current_state == 'q0':
+            self.current_state
